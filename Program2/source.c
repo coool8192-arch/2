9a888de375d1637374c4726d8db897ce99f9c217
coool8192-arch <coool8192@gmail.com>
2026-01-15 21:44:14 +0900

    260115 이중 버퍼링

--- Program2/source.c
+++ Program2/source.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define size 1000
+
+int fs(FILE* file)
+{
+	int c = 0;
+	int s = 0;
+
+	while ((c = fgetc(file)) != EOF)
+	{
+		s++;
+	}
+	rewind(file);
+	return s;
+}
+
 int main()
 {
 #pragma region 파일 입출력
@@ -9,13 +24,37 @@ int main()
 	//매개 변수 2(파일의 입출력 모드)
 
 
-	FILE* file = fopen("data.txt","w");
+	//FILE* file = fopen("data.txt","w");
+	//
+	//fputs("Character Information\n", file);
+	//
+	//fputs("Health : \n", file);
+	//fputs("Attack : \n", file);
+	//fputs("Defence : \n", file);
+	//
+	//fclose(file);
 
-	fputs("Character Information\n", file);
+#pragma endregion
 
-	fputs("Health : \n", file);
-	fputs("Attack : \n", file);
-	fputs("Defence : \n", file);
+#pragma region 파일 읽기
+
+	FILE* file = fopen("data.txt", "r");
+
+	int filesize = fs(file);
+
+	printf("파일의 크기 : %d\n", filesize);
+	
+	
+	char *buffer = calloc(filesize + 1,1);
+
+	//매개 변수 1(저장할 메모리 버퍼의 포인터 변수)
+	//매개 변수 2(각 데이터 항목의 크기)
+	//매개 변수 3(읽어올 데이터 항목의 개수)
+	//매개 변수 4(읽어올 파일의 포인터 변수)
+
+	fread(buffer, sizeof(char), sizeof(buffer), file);
+
+	printf("%s", buffer);
 
 	fclose(file);
 
