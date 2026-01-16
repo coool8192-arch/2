2b0d71867d5958c53523e80fb42a4500cadc33bf
coool8192-arch <coool8192@gmail.com>
2026-01-16 21:44:17 +0900

    260116 색상 시스템

--- Program2/source.c
+++ Program2/source.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-#define size 1000
+#include <stdlib.h>
 
 int fs(FILE* file)
 {
@@ -15,6 +14,23 @@ int fs(FILE* file)
 	return s;
 }
 
+void load(const char* filename)
+{
+	FILE* f = fopen(filename, "r");
+
+	int filesize = fs(f);
+
+	char* buf = calloc(filesize + 1, 1);
+
+	size_t r = fread(buf, sizeof(*buf), filesize, f);
+
+	buf[r] = '\0';
+
+	printf("%s", buf);
+
+	free(buf);
+	fclose(f);
+}
 int main()
 {
 #pragma region 파일 입출력
@@ -38,25 +54,28 @@ int main()
 
 #pragma region 파일 읽기
 
-	FILE* file = fopen("data.txt", "r");
-
-	int filesize = fs(file);
-
-	printf("파일의 크기 : %d\n", filesize);
-	
-	
-	char *buffer = calloc(filesize + 1,1);
-
-	//매개 변수 1(저장할 메모리 버퍼의 포인터 변수)
-	//매개 변수 2(각 데이터 항목의 크기)
-	//매개 변수 3(읽어올 데이터 항목의 개수)
-	//매개 변수 4(읽어올 파일의 포인터 변수)
-
-	fread(buffer, sizeof(char), sizeof(buffer), file);
-
-	printf("%s", buffer);
-
-	fclose(file);
+	//FILE* file = fopen("아스키 블래키.txt", "r");
+	//
+	//int filesize = fs(file);
+	//
+	//printf("파일의 크기 : %d\n", filesize);
+	//
+	//char *buffer = calloc(filesize + 1,1);
+	//
+	////매개 변수 1(저장할 메모리 버퍼의 포인터 변수)
+	////매개 변수 2(각 데이터 항목의 크기)
+	////매개 변수 3(읽어올 데이터 항목의 개수)
+	////매개 변수 4(읽어올 파일의 포인터 변수)
+	//
+	//int r=fread(buffer, 1, filesize, file);
+	//buffer[r] = '\0';
+	//
+	//printf("%s", buffer);
+	//
+	//free(buffer);
+	//fclose(file);
+	//
+	//load("resource/아스키 블래키.txt");
 
 #pragma endregion
 
